f9eedcf74523915d7c6554a86bdca2f7ffacacd2
Brandon Jun <arachnite.b@gmail.com>
2025-09-01 23:51:47 -0700

    feat(robotcontainer): implement and integrate malefix

--- src/main/java/frc/robot/RobotContainer.kt
+++ src/main/java/frc/robot/RobotContainer.kt
@@ -1,6 +1,7 @@
 package frc.robot
 
 import com.pathplanner.lib.commands.PathPlannerAuto
+import edu.wpi.first.wpilibj.XboxController
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.button.JoystickButton
 import frc.robot.commands.Kommand.drive
@@ -10,6 +11,18 @@ import frc.robot.subsystems.Swerve
 import frc.robot.utils.RobotParameters.SwerveParameters.Thresholds
 import frc.robot.utils.controller.GamingController
 import org.littletonrobotics.junction.networktables.LoggedDashboardChooser
+import xyz.malefic.frc.emu.Button
+import xyz.malefic.frc.emu.Button.A
+import xyz.malefic.frc.emu.Button.B
+import xyz.malefic.frc.emu.Button.X
+import xyz.malefic.frc.emu.Button.Y
+import xyz.malefic.frc.emu.Button.START
+import xyz.malefic.frc.emu.Button.DPAD_UP
+import xyz.malefic.frc.emu.Button.LEFT_BUMPER
+import xyz.malefic.frc.emu.Button.LEFT_TRIGGER
+import xyz.malefic.frc.emu.Button.RIGHT_BUMPER
+import xyz.malefic.frc.emu.Button.RIGHT_STICK
+import xyz.malefic.frc.pingu.Bingu.bindings
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -17,33 +30,18 @@ import org.littletonrobotics.junction.networktables.LoggedDashboardChooser
  * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
  * subsystems, commands, and trigger mappings) should be declared here.
  */
-class RobotContainer {
-    private val padA: JoystickButton
-    private val padB: JoystickButton
-    private val padX: JoystickButton
-    private val padY: JoystickButton
-    private val padStart: JoystickButton
-    private val padLeftBumper: JoystickButton
-    private val padRightBumper: JoystickButton
+object RobotContainer {
+
+    val pad: XboxController = XboxController(1)
 
     var networkChooser: LoggedDashboardChooser<Command?> = LoggedDashboardChooser<Command?>("AutoChooser")
 
     /** The container for the robot. Contains subsystems, OI devices, and commands.  */
     init {
-        val pad = GamingController(0)
-        padStart = JoystickButton(pad, 8)
-        padA = JoystickButton(pad, 1)
-        padB = JoystickButton(pad, 2)
-        padX = JoystickButton(pad, 3)
-        padY = JoystickButton(pad, 4)
-        padLeftBumper = JoystickButton(pad, 5)
-        padRightBumper = JoystickButton(pad, 6)
-
-        Swerve.defaultCommand = drive(pad, Thresholds.IS_FIELD_ORIENTED)
-
-        configureBindings()
 
         networkChooser.addDefaultOption("Do Nothing", PathPlannerAuto("Straight Auto"))
+
+        configureBindings()
     }
 
     /**
@@ -52,8 +50,10 @@ class RobotContainer {
      * the named factories in [edu.wpi.first.wpilibj2.command.button.CommandGenericHID]'s subclasses for [edu.wpi.first.wpilibj2.command.button.CommandXboxController]/[edu.wpi.first.wpilibj2.command.button.CommandPS4Controller] controllers or [edu.wpi.first.wpilibj2.command.button.CommandJoystick].
      */
     private fun configureBindings() { // TODO: Remap bindings
-        padStart.onTrue(resetPidgey())
-        padY.onTrue(setTelePid())
+        pad.bindings {
+            press(Y) { setTelePid() }
+            press(START) { resetPidgey() }
+        }
     }
 
     val autonomousCommand: Command?
